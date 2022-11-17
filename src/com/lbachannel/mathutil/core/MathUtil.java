58edd525fa06708c27b6f7b640386caa26be91d9
lbachannel <anlb19749@gmail.com>
2022-11-17 23:01:41 +0700

    refactor the main code and green forever always

--- src/com/lbachannel/mathutil/core/MathUtil.java
+++ src/com/lbachannel/mathutil/core/MathUtil.java
@@ -20,17 +20,42 @@ public class MathUtil {
         bài này quy ước tính n! trong khoảng từ 0..20
     */
     
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20){
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0..20"); // nếu đưa cà chớn, quăng ra 1 cái ngoại lệ, ko thông báo gì cả
+//        }
+//        if(n == 0 || n == 1){
+//            return 1; // kết thúc cuộc chơi sớm, nếu nhận những đầu vào đặc biệt
+//        }
+//        long product = 1; // tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt
+//        for(int i = 2; i <= n; ++i){
+//            product *= i;
+//        }
+//        return product;
+//    } 
+    
+    // Đệ quy: hiện tượng gọi lại chính mình với 1 quy mô khác - recursion
+    /*
+        n! = 1.2.3.4.5.6. ... .n
+        6! = 6 x 5!
+        5! = 5 x 4!
+        4! = 4 x 3!
+        3! = 3 x 2!
+        2! = 2 x 1! // stop
+    
+        Quy ước: 1! = 0! = 1;
+    
+        Công thức đệ quy: n! = n * (n - 1)!
+    */
+    
     public static long getFactorial(int n){
         if(n < 0 || n > 20){
-            throw new IllegalArgumentException("Invalid argument. N must be between 0..20"); // nếu đưa cà chớn, quăng ra 1 cái ngoại lệ, ko thông báo gì cả
+            throw new IllegalArgumentException("n must be between 0..20");
         }
         if(n == 0 || n == 1){
             return 1; // kết thúc cuộc chơi sớm, nếu nhận những đầu vào đặc biệt
         }
-        long product = 1; // tích nhân dồn, thuật toán heo đất, ốc bu dồn thịt
-        for(int i = 2; i <= n; ++i){
-            product *= i;
-        }
-        return product;
+        
+        return n * getFactorial(n - 1);
     } 
 }
